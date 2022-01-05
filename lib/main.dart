01e4aa1a89fd4073ccd367859863733a4e3a9fd0
kevin imaho <kevin.imaho@gmail.com>
2022-01-05 11:59:29 +0400

    exo defis

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.teal[900],
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.amber,
+                  width: 100.0,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                    width: 100.0,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 100.0,
+                            height: 100.0,
+                            color: Colors.redAccent,
+                          ),
+                          Container(
+                            width: 100.0,
+                            height: 100.0,
+                            color: Colors.black,
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  width: 100.0,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
